34307d04a050dd834382ea220dfe12232773debd
Sam Nazari <sam.nazari@gmail.com>
2018-03-09 10:56:18 -0500

    adding jupyter start script

---
+++ startJupyter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#docker run -i -t -p 8888:8888 -v /Users/samnazari/Dockerfiles/gensimDocker/vols:/dataVol gensim_top_models:latest /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --allow-root --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser"
+docker run -i -t -p 8888:8888 -v /Users/samnazari/Dockerfiles/gensimDocker/vols:/dataVol gensim_top_models:latest /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --allow-root --notebook-dir=/ --ip='*' --port=8888 --no-browser"
